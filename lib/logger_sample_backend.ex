2be63759b97d029f2e61c1a163287f141124171a
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2018-05-08 09:57:31 +0900

    remove initial codes

--- lib/logger_sample_backend.ex
+++ lib/logger_sample_backend.ex
@@ -2,17 +2,4 @@ defmodule LoggerSampleBackend do
   @moduledoc """
   Documentation for LoggerSampleBackend.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> LoggerSampleBackend.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/logger_sample_backend_test.exs
+++ test/logger_sample_backend_test.exs
@@ -1,8 +1,4 @@
 defmodule LoggerSampleBackendTest do
   use ExUnit.Case
   doctest LoggerSampleBackend
-
-  test "greets the world" do
-    assert LoggerSampleBackend.hello() == :world
-  end
 end
