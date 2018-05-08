ee787341a4e9987d49a68522f867045ee866c2ff
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2018-05-08 11:17:48 +0900

    implement a logger backend

--- lib/logger_sample_backend.ex
+++ lib/logger_sample_backend.ex
@@ -1,5 +1,37 @@
 defmodule LoggerSampleBackend do
-  @moduledoc """
-  Documentation for LoggerSampleBackend.
-  """
+  @behaviour :gen_event
+  @path "./log/#{Mix.env()}.log"
+  @format "$date $time [$level] $message"
+
+  def init(__MODULE__) do
+    {:ok, %{path: @path, format: @format}}
+  end
+
+  def handle_call({:configure, opts}, state) do
+    path = Keyword.get(opts, :path, @path)
+    format = Keyword.get(opts, :format, @format)
+    new_state = %{state | path: path, format: format}
+    {:ok, {:ok, new_state}, new_state}
+  end
+
+  def handle_event({level, _group_leader, {Logger, message, timestamp, metadata}}, state) do
+    state.path |> Path.dirname() |> File.mkdir_p()
+
+    log_line =
+      Logger.Formatter.format(
+        Logger.Formatter.compile(state.format),
+        level,
+        message,
+        timestamp,
+        metadata
+      )
+
+    File.write(state.path, "#{log_line}\n", [:append])
+
+    {:ok, state}
+  end
+
+  def handle_event(:flush, state) do
+    {:ok, state}
+  end
 end
